32e0e07754724dc79e2a54ec4e88173cc51a02fe
gimunkwon <gimon71@gmail.com>
2026-03-24 20:45:13 +0900

    [M_Player.h 컴포넌트 포인터 선언]
    USpringArmComponent및 UCameraComponent 포인터 변수 선언및
    해당 클래스를 전방선언 했습니다.

--- Source/CommitPractice/Public/M_Player.h
+++ Source/CommitPractice/Public/M_Player.h
@@ -4,23 +4,23 @@
 #include "GameFramework/Character.h"
 #include "M_Player.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class COMMITPRACTICE_API AM_Player : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	
 	AM_Player();
-
 protected:
-	
 	virtual void BeginPlay() override;
-
+	UPROPERTY(VisibleAnywhere,Category="Camera|SpringArm")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+	UPROPERTY(VisibleAnywhere, Category="Camera")
+	TObjectPtr<UCameraComponent> CameraComp;
 public:
-	
 	virtual void Tick(float DeltaTime) override;
-
-	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
